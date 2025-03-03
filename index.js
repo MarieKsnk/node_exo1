6d4e22f64310bc8edfab15906f0ebca974d60217
Marie Klisnick <marie63.klisnick@gmail.com>
2025-03-03 16:09:37 +0100

    Utilisation d'axios

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import chalk from "chalk";
 import http from "http";
 import fs from "fs";
+import axios from "axios";
 
 const PORT = 8000;
 const localhost = "localhost";
 
-fs.writeFileSync("newFile.txt", "Nouveau fichier");
-
-fs.readFile("newFile.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error("Erreur", err);
-  } else {
-    console.log(data);
-  }
+fs.writeFile("newFile.txt", "Nouveau fichier", (err) => {
+  if (err) console.error(err);
+  else console.log("File is create");
 });
 
-console.log(chalk.magenta("Marie"));
-console.log(chalk.blue("Coralie"));
-console.log(chalk.green("Fadia"));
-console.log(chalk.red("Julie"));
+fs.readFile("./newFile.txt", "utf8", (err, data) => {
+  if (err) console.error(err);
+  else console.log(data);
+});
+
+const fetchGoogle = async () => {
+  try {
+    const response = await axios.get(`https://www.google.com`);
+    fs.writeFile("google.html", response.data, (err) => {
+      if (err) console.error(err);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fetchGoogle();
 
 const server = http.createServer((request, response) => {
   response.end("<h1>Hello world</h1>");
